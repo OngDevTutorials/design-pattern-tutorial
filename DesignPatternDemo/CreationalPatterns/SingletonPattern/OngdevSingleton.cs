8bd6a0a2b4184e210386910709cede522f30141d
DuyNguyen <milo@DuyNguyens-MacBook-Pro.local>
2019-06-16 21:23:51 +0700

    Singleton

---
+++ DesignPatternDemo/CreationalPatterns/SingletonPattern/OngdevSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace SingletonPattern
+{
+    public class OngdevSingleton
+    {
+        private int index;
+        private static readonly object lockObject = new object();
+        private static volatile OngdevSingleton uniqueInstance;
+
+        private OngdevSingleton(int index)
+        {
+            this.index = index;
+        }
+
+        public static OngdevSingleton GetInstance()
+        {
+            if(uniqueInstance == null)
+            {
+                lock (lockObject)
+                {
+                    if (uniqueInstance == null)
+                    {
+                        var random = new Random();
+
+                        uniqueInstance = new OngdevSingleton(random.Next(1, 4));
+                    }
+                }
+            }
+            return uniqueInstance;
+        }
+
+        public void SaysHi()
+        {
+            Console.WriteLine("Hello everyone, I am Ongdev number " + index);
+        }
+    }
+}
---
+++ DesignPatternDemo/CreationalPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread1 = new Thread(() => OngdevSingleton.GetInstance().SaysHi());
+            var thread2 = new Thread(() => OngdevSingleton.GetInstance().SaysHi());
+
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
